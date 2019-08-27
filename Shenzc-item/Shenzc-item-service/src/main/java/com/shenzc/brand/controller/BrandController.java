01b71b7b7506aaad76d3ae732a4c5d031782d620
shenzc <1054694244@qq.com>
2019-08-27 16:49:40 +0800

    brand

--- Shenzc-item/Shenzc-item-service/src/main/java/com/shenzc/brand/controller/BrandController.java
+++ Shenzc-item/Shenzc-item-service/src/main/java/com/shenzc/brand/controller/BrandController.java
@@ -2,9 +2,12 @@ package com.shenzc.brand.controller;
 
 
 import com.shenzc.brand.service.BrandService;
+import com.shenzc.common.PageResult;
+import com.shenzc.entity.Brand;
 import com.shenzc.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,12 +26,17 @@ public class BrandController {
     private BrandService brandService;
 ///brand/page?key=&page=1&rows=5&sortBy=id&desc=false
     @GetMapping("/brand/page")
-    public ResponseEntity<List<Category>> findAllBrand(@RequestParam(value = "key",defaultValue = "") String key,
-                                                       @RequestParam(value = "page",defaultValue = "") int page,
-                                                       @RequestParam(value = "rows",defaultValue = "") int rows,
-                                                       @RequestParam(value = "sortBy",defaultValue = "") int sortBy,
-                                                       @RequestParam(value = "desc",defaultValue = "") boolean desc){
-            return  null;
+    public ResponseEntity<PageResult<Brand>> findAllBrand(@RequestParam(value = "key",defaultValue = "") String key,
+                                                       @RequestParam(value = "page",defaultValue = "1") Integer page,
+                                                       @RequestParam(value = "rows",defaultValue = "5") Integer rows,
+                                                       @RequestParam(value = "sortBy",defaultValue = "") String sortBy,
+                                                       @RequestParam(value = "desc",defaultValue = "false") boolean desc){
+        PageResult<Brand> result = brandService.findByAllBrand(page, rows, sortBy, desc, key);
+        if (null == result || result.getItems().size() == 0){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            return ResponseEntity.ok(result);
+        }
     }
 
 }
--- Shenzc-item/Shenzc-item-service/src/main/java/com/shenzc/brand/service/BrandService.java
+++ Shenzc-item/Shenzc-item-service/src/main/java/com/shenzc/brand/service/BrandService.java
@@ -1,12 +1,16 @@
 package com.shenzc.brand.service;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.shenzc.brand.mapper.BrandMapper;
+import com.shenzc.common.PageResult;
 import com.shenzc.entity.Brand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author 婵大pig
@@ -18,9 +22,20 @@ public class BrandService {
     @Autowired
     private BrandMapper brandMapper;
 
-    public List<Brand> findByAllBrand(){
+    public PageResult<Brand> findByAllBrand(Integer page, Integer rows, String sortBy, Boolean desc, String key){
+        if (desc){
+            PageHelper.startPage(page,rows,sortBy);
+        }else {
+            PageHelper.startPage(page,rows);
+        }
         List<Brand> brandList = brandMapper.selectList(new EntityWrapper<Brand>());
-        return brandList;
+        List<Brand> list = brandList.parallelStream().filter(
+                brand ->
+                    brand.getName().contains(key) || brand.getLetter().equals(key)
+                )
+                .collect(Collectors.toList());
+        PageInfo<Brand> pageInfo = new PageInfo<>(list);
+        return new PageResult<Brand>(pageInfo.getTotal(),pageInfo.getList());
     }
 
 }
